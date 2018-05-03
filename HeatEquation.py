17cc941b82d6b363a9a8fc155f432f4c7b3e8822
imcdo <ian_mcdonald@rocketmail.com>
2018-05-03 13:42:35 -0500

    basic structure added

---
+++ HeatEquation.py
@@ -0,0 +1,39 @@
+import numpy as np
+import scipy.integrate as integrate
+import matplotlib.pyplot as plt
+import fractions
+
+def heat_equation(alpha_sqrd, l, f):
+    """
+    returns the function for the heat equation, 
+    this function takes in the parms t time and x
+    pos
+    
+    Arguments:
+        alpha_sqrd {number} -- the alpha squared
+        l {number} -- length of bar
+        f {function} -- function f(x)
+    """ 
+
+    def b(n):
+        """calculates the b(n) for the heat equation
+        
+        Arguments:
+            n {int} -- possition in the n
+        """
+        pass
+
+    pass
+
+
+def graph_heat_equation(heat_eq):
+    raise NotImplementedError
+    
+
+def tests():
+    l = 2
+    alpha_sqrd = 2
+    f = lambda x : x
+    raise NotImplementedError
+
+tests()
